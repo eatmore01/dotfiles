74958c959d4a8a4d7d5206ee2c60e264c5c33c29
eatmore01 <kirillzuev987@gmail.com>
2026-01-19 07:05:47 +0800

    JSAS

--- macos.sh
+++ macos.sh
@@ -1,2 +1,6 @@
 go install github.com/google/yamlfmt/cmd/yamlfmt@latest
 brew install alesbrelih/gitlab-ci-ls/gitlab-ci-ls
+
+
+
+# defaults write com.apple.finder QuitMenuItem -bool YES && killall Finder
