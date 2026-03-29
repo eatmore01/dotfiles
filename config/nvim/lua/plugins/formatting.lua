fa34a1c22fb416997351d1a0b798a1dadcdc1028
eatmore01 <etm@etm.com>
2026-03-29 13:27:40 +0300

    ---

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -1,109 +1,110 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local conform = require("conform")
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
 
-            conform.setup({
-                formatters_by_ft = {
-                    yaml = { "yamlfmt" },
-                    yml = { "yamlfmt" },
-                    sh = { "shfmt" },
-                    bash = { "shfmt" },
-                    go = { "goimports", "gofumpt" },
-                    terraform = { "terraform_fmt" },
-                    hcl = { "terraform_fmt" },
-                    tf = { "terraform_fmt" },
-                    json = { "prettier" },
-                    lua = { "stylua" },
-                    -- markdown = { "prettier" },
-                },
+			conform.setup({
+				formatters_by_ft = {
+					yaml = { "yamlfmt" },
+					yml = { "yamlfmt" },
+					sh = { "shfmt" },
+					bash = { "shfmt" },
+					go = { "goimports", "gofumpt" },
+					terraform = { "terraform_fmt" },
+					hcl = { "terraform_fmt" },
+					tf = { "terraform_fmt" },
+					json = { "prettier" },
+					lua = { "stylua" },
+					-- markdown = { "prettier" },
+				},
 
-                linters_by_ft = {
-                    terraform = { "tflint", "tfsec" },
-                },
+				linters_by_ft = {
+					terraform = { "tflint", "tfsec" },
+					yaml = { "kube-linter", "kubescape" },
+				},
 
-                -- format on save
-                format_on_save = {
-                    timeout_ms = 500,
-                    lsp_fallback = true,
-                },
+				-- format on save
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_fallback = true,
+				},
 
-                formatters = {
-                    yamlfmt = {
-                        prepend_args = {
-                            "-formatter",
-                            "indent=2",
-                            "-formatter",
-                            "indentless_arrays=true", -- list without additional indent
-                            "-formatter",
-                            "retain_line_breaks=true",
-                            "-formatter",
-                            "max_line_length=80",
-                        },
-                    },
-                },
+				formatters = {
+					yamlfmt = {
+						prepend_args = {
+							"-formatter",
+							"indent=2",
+							"-formatter",
+							"indentless_arrays=true", -- list without additional indent
+							"-formatter",
+							"retain_line_breaks=true",
+							"-formatter",
+							"max_line_length=80",
+						},
+					},
+				},
+			})
+			-- maunal save
+			vim.keymap.set({ "n", "v" }, "<leader>fmt", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end, { desc = "Format file or range" })
+		end,
+	},
 
-            })
-            -- maunal save
-            vim.keymap.set({ "n", "v" }, "<leader>fmt", function()
-                conform.format({
-                    lsp_fallback = true,
-                    async = false,
-                    timeout_ms = 1000,
-                })
-            end, { desc = "Format file or range" })
-        end,
-    },
+	-- autoinstall formattets
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"prettier",
+					"yamlfmt",
+					"shfmt",
+					"goimports",
+					"gofumpt",
+					"stylua",
+					"yamllint",
+					"shellcheck",
+					"golangci-lint",
+					-- terraform
+					-- lintiongs
 
-    -- autoinstall formattets
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-        config = function()
-            require("mason-tool-installer").setup({
-                ensure_installed = {
-                    "prettier",
-                    "yamlls",
-                    "yamlfmt",
-                    "shfmt",
-                    "goimports",
-                    "gofumpt",
-                    "stylua",
-                    "yamllint",
-                    "shellcheck",
-                    "golangci-lint",
-                    -- terraform
-                    -- lintiongs
+					"tflint",
+					"tfsec", -- Security scan
 
-                    "tflint",
-                    "tfsec", -- Security scan
+					-- kubelinters
+					"kube-linter",
+					"kubescape",
+				},
 
-                    -- LSP
-                    "terraformls",
-                },
-                auto_update = true,
-                run_on_start = true,
-            })
-        end,
-    },
+				auto_update = true,
+				run_on_start = true,
+			})
+		end,
+	},
 
-    {
-        "mfussenegger/nvim-lint",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local lint = require("lint")
-            lint.linters_by_ft = {
-                terraform = { "tflint", "tfsec" },
-                tf = { "tflint", "tfsec" },
-            }
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				terraform = { "tflint", "tfsec" },
+				tf = { "tflint", "tfsec" },
+			}
 
-            vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
-                callback = function()
-                    lint.try_lint()
-                end,
-            })
-        end,
-    },
+			vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+		end,
+	},
 }
