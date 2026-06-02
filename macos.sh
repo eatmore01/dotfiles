f112e284cf331cac550136abe95ecf928855ee4d
eatmore01 <155847582+eatmore01@users.noreply.github.com>
2026-06-02 13:19:43 +0300

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -5,3 +5,7 @@ git config --global core.excludesfile ~/.gitignore_global
 #
 #
 # defaults write com.apple.finder QuitMenuItem -bool YES && killall Finder
+
+# stop creatin .DS_Store file
+# https://johnhalz.github.io/how_to/operating_systems_and_configs/stop_ds_store_file_creation/
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
